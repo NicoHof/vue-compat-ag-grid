dcbb7f0ab0664da4f64d9a40e0122d684c1f6079
Nicolas Hofmair <n.hofmair.ext@meteocontrol.com>
2024-03-18 17:31:09 +0100

    Initial

---
+++ README.md
@@ -0,0 +1,18 @@
+# setup
+
+```
+yarn install
+```
+
+
+## start vue in compat mode
+
+```
+yarn serve:compat
+```
+
+## start vue3 without compat mode
+
+```
+yarn serve:vue3
+```
---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h1>AG Grid</h1>
+        <GridExample />
+    </div>
+</template>
+
+<script>
+import GridExample from './components/GridExample';
+
+export default {
+    name: 'App',
+    compatConfig: {
+        MODE: 3,
+    },
+    components: {
+        GridExample,
+    },
+};
+</script>
+
+<style>
+#app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
